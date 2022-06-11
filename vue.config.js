0b814e2c82fe54c146e9c26edf003c6c28222c9d
David Diamant <david@homelend.com>
2022-06-11 19:24:11 +0300

    add server access

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    proxy: {
+      "/api/": {
+        target: "http://localhost:9000/api/"
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
