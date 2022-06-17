115c1f31efaf6fc8b5f427738af7e73c8e1b6295
David Diamant <david@homelend.com>
2022-06-17 13:58:00 +0300

    remove comments

--- public/server.js
+++ public/server.js
@@ -1,8 +1,3 @@
-// require("http").createServer(function (req, res) {
-//     res.end("Hello from server started by Electron app!");
-// }).listen(9000)
-
-
 const app = require("./app");
 const port = process.env.PORT || 9000;
 
